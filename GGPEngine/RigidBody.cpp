67390bf4b5ba0760355092c176d4b39f70533c84
idontmakesense <mj9617@g.rit.edu>
2015-03-26 16:21:04 -0400

    Changed structure and created one rigidbody class. Calculates velocity, angular velocity and spin.

--- GGPEngine/RigidBody.cpp
+++ GGPEngine/RigidBody.cpp
@@ -3,6 +3,16 @@
 
 RigidBody::RigidBody()
 {
+	size = 1;
+	mass = 1;
+	inverseMass = 1.0f / mass;
+	position = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	momentum = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	orientation = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
+	angularMomentum = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	inertia = mass * size * size * 1.0f / 6.0f;
+	inverseInertia = 1.0f / inertia;
+	recalculate();
 }
 
 
@@ -10,4 +20,34 @@ RigidBody::~RigidBody()
 {
 }
 
-// calc velocity from momentum and inverse mass
+void RigidBody::recalculate()
+{
+	// load all vectors
+	XMVECTOR Momentum = XMLoadFloat3(&momentum);
+	XMVECTOR Velocity = XMLoadFloat3(&velocity);
+
+	XMVECTOR AngularMomentum = XMLoadFloat3(&angularMomentum);
+	XMVECTOR AngularVelocity = XMLoadFloat3(&angularVelocity);
+
+	XMVECTOR Orientation = XMLoadFloat4(&orientation);
+	XMVECTOR Spin = XMLoadFloat4(&spin);
+
+	// calculate velocity
+	Velocity = Momentum * inverseMass;
+
+	// calculate angular velocity
+	AngularVelocity = AngularMomentum * inverseInertia;
+
+	// normalize orientation
+	Orientation = XMQuaternionNormalize(Orientation);
+
+	// calculate spin
+	Spin = 0.5f * XMVectorSet(angularVelocity.x, angularVelocity.y, angularVelocity.z, 0) * Orientation;
+
+	// store all calculated vectors 
+	XMStoreFloat3(&velocity, Velocity);
+
+	XMStoreFloat3(&angularVelocity, AngularVelocity);
+
+	XMStoreFloat4(&spin, Spin);
+}
