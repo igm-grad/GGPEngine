e196beabd2392a3759b49efeccfa922e45162706
unknown <go4113@GDD-62.main.ad.rit.edu>
2015-05-03 19:15:41 -0400

    Project Settings and file adds.

---
+++ Demo04_Materials/DiffuseFluidVertexShader.hlsl
@@ -0,0 +1,37 @@
+struct Vertex
+{
+	float4 tangent	: TANGENT;
+	float3 position : POSITION;
+	float3 normal	: NORMAL;
+	float2 uv		: TEXCOORD;
+};
+
+struct Pixel
+{
+	float4 position	: SV_POSITION;
+	float3 normal	: NORMAL;
+	float3 positionT: POSITIONT;
+	float2 uv		: TEXCOORD;
+};
+
+cbuffer transform : register(b0)
+{
+	matrix world;
+	matrix view;
+	matrix projection;
+	double time;
+}
+
+Pixel main(Vertex vertex)
+{
+	matrix clip = mul(mul(world, view), projection);
+
+	Pixel pixel;
+	vertex.position += vertex.normal * sin(time * 100000.0);
+	pixel.position = mul(float4(vertex.position, 1.0f), clip);
+	pixel.normal = mul(vertex.normal, (float3x3)world);
+	pixel.positionT = mul(float4(vertex.position, 1.0f), world).xyz;
+	pixel.uv = vertex.uv;
+
+	return pixel;
+}
